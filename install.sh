118410253857f9af670e2273f1813e7529748d6c
Daniel Scally <djrscally@gmail.com>
2018-11-17 18:51:17 +0000

    Almost finished the install.sh

--- install.sh
+++ install.sh
@@ -6,71 +6,83 @@
 # data.
 
 
-MYSQL_BIN=/usr/bin/mysqlx # Remove the x for production
-CONFIG_FILE=./config.jsonx # Remove the x for production
+MYSQL_BIN=/usr/bin/mysql
+PYTHON_BIN=/usr/bin/python3
+CONFIG_FILE=./config.json
+JQ_BIN=/usr/bin/jq
 
-function buildconfig {
-
-	# First we'll collect the info we need to generate the config
-
-	read -p "Enter the Database host: " dbhost_var
-	read -p "Enter the Database user: " dbuser_var
-	read -sp "Enter the Database password: " dbpass_var
-	read -p "Enter the PUBG shard you play in: " shard_var
-	read -p "Paste in your PUBG API key: " api_var
-
-	players=()
-
-	# Players is a little complex as there could be any number. We'll do a simple
-	# while loop to check if the person running the script is done adding their
-	# players or whether to continue.
-
-	players+=(`read -p "Enter the first player's name: "`)
-
-	read -p "Are there any more players to add? (y/n): " exit_loop
-
-	while [ "$exit_loop" == "y" ];
-		do
-			players+=(`read -p "Enter the next player's name: "`)
-			read -p "Are there any more player's to add? (y/n): " exit_loop
-		done
-
-	# So we create the config file and change the permissions to 700, since there's some
-	# sensitive information in here.
-
-	touch $CONFIG_FILE
-	chmod 700 $CONFIG_FILE
-
-	# Now we drop in all the values we collected to create a json files
-	echo $players
-	jq -n --argjson v $players '{"players": [$v]}' > $CONFIG_FILE
-
-	return 0
+# First a helper function to check that the config file actually exists!
+checkconfig(){
+	if [ ! -r "$CONFIG_FILE" ]; then
+		read -p "No config file found. Would you like to create one now? (y/n) " create_var
+		if [ "$create_var" == "y" ]; then
+			$PYTHON_BIN ./config.py
+		else
+			echo "This installation requires a config file. Please re-run the script later"
+			exit 1
+		fi
+	else
+		echo "Confile file detected."
+	fi
 }
 
-# Step 1. Check if all the binaries we need to exist actually do exist. If not,
-# list the missing requirements and demand they go install them.
+# Install routine First
 
+install() {
 
+	# Step 1. Check if the programs we need are actually installed, and if not
+	# send them to fix that.
 
-# Step 2. Check if config.json exists and build it if not.
-if [ ! -r "$CONFIG_FILE" ]; then
-	read -p "No config file available. Would you like to create one now? (y/n)" create_var
-	if [ "$create_var" == "y" ]; then
-		buildconfig
-	else
-		echo "This installation requires a config file. Please re-run the script later"
+	if [ ! -x "$MYSQL_BIN" ]; then
+		echo "ERROR: MySQL is not installed or not in the usual place. Please either \
+ install it or edit this script to provide the appropriate path to the \
+ MYSQL_BIN variable"
 		exit 1
 	fi
-else
-	echo "Confile file detected."
-fi
+
+	if [ ! -x "$PYTHON_BIN" ]; then
+		echo "ERROR: Python3 is not installed or not in the usual place. Please either\
+install it or edit this script to provide the appropriate path to the PYTHON_BIN\
+variable"
+		exit 1
+	fi
+
+	if [ ! "$JQ_BIN" ]; then
+		echo "ERROR: jq is not installed. jq is a terminal tool for parsing JSON, please\
+ install it and re-run install.sh"
+ 		exit 1
+	fi
+
+	echo "All dependencies met..."
+
+	# Step 2. Check if config.json exists and build it if not.
+
+	checkconfig
+
+	# Load the database login
+	DB_UN=`jq ".db_un" config.json`
+	DB_PW=`jq ".db_pw" config.json`
+
+	# run the create_db.sql script to create the database
+	$MYSQL_BIN -u "$DB_UN" -p "$DB_PW" < database/create_db.sql
+	# run the sync.py script to fill it with data
+	$PYTHON_BIN ./sync.py
+	# create a cron job to automate the script to run every night.
+
+}
 
 
-if [ ! -x "$MYSQL_BIN" ]; then
-	echo "ERROR: MySQL is missing or not in the usual place. Please either \
-install it or edit this script to provide the appropriate path to the \
-MYSQL_BIN variable"
-else
-	echo "MySQL Present and OK."
-fi
+case "$1" in
+	install)
+		install
+	;;
+	uninstall)
+		echo "UNINSTALL!!"
+	;;
+	reload)
+		echo "RELOAD!!"
+	;;
+	*)
+		echo "Usage: $0 {install|uninstall|reload}"
+esac
+echo "$1 operation complete."
