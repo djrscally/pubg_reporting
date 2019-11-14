5d18a5c7dfc7235ee35ed73cf7426ba59b2a3305
Dan Scally <Dan.Scally@Insight.com>
2019-11-14 14:39:45 +0000

    Adding more tables to the ORM expansion

--- database/pubgdb.py
+++ database/pubgdb.py
@@ -1,5 +1,12 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy import \
+    Column\
+    , Integer\
+    , String\
+    , Boolean\
+    , DateTime\
+    , ForeignKey\
+    , Table
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
@@ -15,7 +22,16 @@ class Player(Base):
     player_name = Column(String, nullable=False)
     shard_id = Column(String, nullable=False)
 
-    matches = relationship('Match')
+    matches = relationship(
+        'Match',
+        secondary=player_matches,
+        back_populates='players'
+    )
+
+    player_season_stats = relationship(
+        'PlayerSeasonStats',
+        back_populates='players'
+    )
 
     def __repr__(self):
         return "<Player(player_id={0}, player_name={1})>".format(
@@ -36,6 +52,11 @@ class Season(Base):
     is_current_season = Column(Boolean, nullable=False)
     is_off_season = Column(Boolean, nullable=False)
 
+    player_season_stats = relationship('PlayerSeasonStats', back_populates='seasons')
+
+    def __repr__(self):
+        return "<Season(season_id={0})".format(self.season_id)
+
 class Match(Base):
     """
     Base class for individual matches, details about the actual rounds
@@ -53,21 +74,83 @@ class Match(Base):
     seasonState = Column(String, nullable=False)
     shardId = Column(String, nullable=False)
 
-class PlayerMatch(Base):
-    """
-    relational table tying players to the matches they have been
-    in, but again no stats.
-    """
-
-    __tablename__ = 'player_matches'
-
-    player_match_id = Column(
-        Integer,
-        primary_key=True,
-        nullable=False,
-        autoincrement=True
+    players = relationship(
+        'Player',
+        secondary=player_matches,
+        back_populates='matches'
     )
-    player_id = Column(String, ForeignKey('players.player_id'))
-    player = relationship('Player', back_populates='player_matches')
-    match_id = Column(String, ForeignKey('matches.match_id'))
-    match = relationship('Match', back_populates='player_matches')
+
+    def __repr__(self):
+        return "<Match(match_id={0}, gameMode={1}, mapName={2})".format(
+            self.match_id,
+            self.gameMode,
+            self.mapName
+        )
+
+player_matches = Table(
+    'player_matches',
+    Base.metadata,
+    Column('player_id', String, ForeignKey('players.player_id')),
+    Column('match_id', String, ForeignKey('matches.match_id'))
+)
+
+season_matches = Table(
+    'season_matches',
+    Base.metadata,
+    Column('season_id', String, ForeignKey('seasons.season_id')),
+    Column('match_id', String, ForeignKey('matches.match_id'))
+)
+
+class PlayerSeasonStats(Base):
+    """
+    Placeholder
+    """
+
+    __tablename__ = 'player_season_stats'
+
+    season_stats_id = Column(Integer, primary_key=True, autoincrement=True)
+
+    season_id = Column(String, nullable=False, ForeignKey('seasons.season_id'))
+    seasons = relationship('Season', back_populates='seasons')
+
+    player_id = Column(String, nullable=False, ForeignKey('players.player_id'))
+    players = relationship('Player', back_populates='players')
+
+    game_mode = Column(String, nullable=False)
+
+    game_mode nvarchar(255) not null
+    assists = Column(Integer, nullable=False)
+    bestRankPoint = Column(Integer, nullable=False)
+    boosts = Column(Integer, nullable=False)
+    dBNOs = Column(Integer, nullable=False)
+    dailyKills = Column(Integer, nullable=False)
+    damageDealt = Column(Integer, nullable=False)
+    days = Column(Integer, nullable=False)
+    dailyWins = Column(Integer, nullable=False)
+    headshotKills = Column(Integer, nullable=False)
+    heals = Column(Integer, nullable=False)
+    killPoints = Column(Integer, nullable=False)
+    kills = Column(Integer, nullable=False)
+    longestKill = Column(Integer, nullable=False)
+    longestTimeSurvived = Column(Integer, nullable=False)
+    losses = Column(Integer, nullable=False)
+    maxKillStreaks = Column(Integer, nullable=False)
+    mostSurvivalTime = Column(Integer, nullable=False)
+    rankPoints = Column(Integer, nullable=False)
+    revives = Column(Integer, nullable=False)
+    rideDistance = Column(Integer, nullable=False)
+    roadKills = Column(Integer, nullable=False)
+    roundMostKills = Column(Integer, nullable=False)
+    roundsPlayed = Column(Integer, nullable=False)
+    suicides = Column(Integer, nullable=False)
+    swimDistance = Column(Integer, nullable=False)
+    teamKills = Column(Integer, nullable=False)
+    timeSurvived = Column(Integer, nullable=False)
+    top10s = Column(Integer, nullable=False)
+    vehicleDestroys = Column(Integer, nullable=False)
+    walkDistance = Column(Integer, nullable=False)
+    weaponsAcquired = Column(Integer, nullable=False)
+    weeklyKills = Column(Integer, nullable=False)
+    weeklyWins = Column(Integer, nullable=False)
+    winPoints = Column(Integer, nullable=False)
+    wins = Column(Integer, nullable=False)
