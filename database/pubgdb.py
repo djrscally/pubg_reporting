9315dabad76c7ea7810f182774ad96c695c8418f
Dan Scally <Dan.Scally@Insight.com>
2019-11-14 13:29:20 +0000

    Continuing work on ORM changes

--- database/pubgdb.py
+++ database/pubgdb.py
@@ -1,18 +1,21 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-class Player(base):
+class Player(Base):
     """
     Defines the players
     """
 
-    __tablename__ = 'users'
+    __tablename__ = 'players'
 
     player_id = Column(String, primary_key=True, nullable=False)
     player_name = Column(String, nullable=False)
     shard_id = Column(String, nullable=False)
+
+    matches = relationship('Match')
 
     def __repr__(self):
         return "<Player(player_id={0}, player_name={1})>".format(
@@ -20,7 +23,7 @@ class Player(base):
             self.player_name
         )
 
-class Season(base):
+class Season(Base):
     """
     Base class for Seasons, which are like a bucket of matches that are very
     inconsistent lengths. Originally once per month, then they went to much
@@ -30,5 +33,41 @@ class Season(base):
     __tablename__ = 'seasons'
 
     season_id = Column(String, primary_key=True, nullable=False)
-    , is_current_season = Column(Boolean, nullable=False)
-    , is_off_season = Column(Boolean, nullable=False)
+    is_current_season = Column(Boolean, nullable=False)
+    is_off_season = Column(Boolean, nullable=False)
+
+class Match(Base):
+    """
+    Base class for individual matches, details about the actual rounds
+    played (although not any stats which is weird)
+    """
+
+    __tablename__ = 'matches'
+
+    match_id = Column(String, primary_key=True, nullable=False)
+    createdAt = Column(DateTime, nullable=False)
+    duration = Column(Integer, nullable=False)
+    gameMode = Column(String, nullable=False)
+    mapName = Column(String, nullable=False)
+    isCustomMatch = Column(Boolean, nullable=False)
+    seasonState = Column(String, nullable=False)
+    shardId = Column(String, nullable=False)
+
+class PlayerMatch(Base):
+    """
+    relational table tying players to the matches they have been
+    in, but again no stats.
+    """
+
+    __tablename__ = 'player_matches'
+
+    player_match_id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True
+    )
+    player_id = Column(String, ForeignKey('players.player_id'))
+    player = relationship('Player', back_populates='player_matches')
+    match_id = Column(String, ForeignKey('matches.match_id'))
+    match = relationship('Match', back_populates='player_matches')
