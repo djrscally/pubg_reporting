48147b9f92b3039bdda2a2d29ac3a8667a62a873
Daniel Scally <djrscally@gmail.com>
2020-02-20 22:27:50 +0000

    enabled click and started to build in the logging

--- sync.py
+++ sync.py
@@ -5,40 +5,62 @@ from pubg.pubg_api import pubg_api
 import json
 import pymysql
 import os
+import click
+import logging
 
-user = os.environ.get('PUBGDB_USERNAME')
-password = os.environ.get('PUBGDB_PASSWORD')
-host = os.environ.get('PUBGDB_HOST')
-database = os.environ.get('PUBGDB_DATABASE')
+@click.command()
+@click.option(
+    '--log-level',
+    'loglevel',
+    type=click.Choice(['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']),
+    default='Warning',
+    help='Level of detail to include in logs')
+def sync(loglevel):
 
-db_uri = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(user, password, host, database)
-#db_uri = 'sqlite:///:memory:'
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    logging.basicConfig(filename='sync.log', filemode='w', level=numeric_level)
 
-pubgdb = PUBGDatabaseConnector(db_uri, echo=True)
-Base.metadata.create_all(pubgdb.engine)
+    user = os.environ.get('PUBGDB_USERNAME')
+    password = os.environ.get('PUBGDB_PASSWORD')
+    host = os.environ.get('PUBGDB_HOST')
+    database = os.environ.get('PUBGDB_DATABASE')
 
-config = json.load(open(os.environ.get('PUBGDB_CONFIG_PATH') + 'config.json'))
+    db_uri = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(user, password, host, database)
+    #db_uri = 'sqlite:///:memory:'
 
-api = pubg_api(config)
+    pubgdb = PUBGDatabaseConnector(db_uri, echo=True)
+    Base.metadata.create_all(pubgdb.engine)
 
-api.get_players()
-pubgdb.upsert_players(api.players)
+    config = json.load(open(os.environ.get('PUBGDB_CONFIG_PATH') + 'config.json'))
+    api = pubg_api(config)
 
-api.get_seasons()
-pubgdb.upsert_seasons(api.seasons)
+    __sync(api, pubgdb)
 
-api.get_matches()
-pubgdb.upsert_matches(api.matches)
+def __sync(api, pubgdb):
+    logging.debug("Beginning get_players() call")
+    api.get_players()
+    logging.debug("Completed get_players() call")
+    pubgdb.upsert_players(api.players)
 
-pubgdb.upsert_player_matches(api.players)
-print("""
-============= Player Match Stats Start ===============
-""")
-pubgdb.upsert_player_match_stats(api.matches, api.players)
+    api.get_seasons()
+    pubgdb.upsert_seasons(api.seasons)
 
-api.get_player_season_stats()
-pubgdb.upsert_player_season_stats(api.player_season_stats)
-pubgdb.upsert_season_matches(api.player_season_stats)
+    api.get_matches()
+    pubgdb.upsert_matches(api.matches)
 
-api.get_player_lifetime_stats()
-pubgdb.upsert_player_lifetime_stats(api.player_lifetime_stats)
+    pubgdb.upsert_player_matches(api.players)
+    print("""
+    ============= Player Match Stats Start ===============
+    """)
+    pubgdb.upsert_player_match_stats(api.matches, api.players)
+
+    api.get_player_season_stats()
+    pubgdb.upsert_player_season_stats(api.player_season_stats)
+    pubgdb.upsert_season_matches(api.player_season_stats)
+
+    api.get_player_lifetime_stats()
+    pubgdb.upsert_player_lifetime_stats(api.player_lifetime_stats)
+
+if __name__=='__main__':
+
+    sync()
